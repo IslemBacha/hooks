6c513b837b38f2459fbe38fecf4ee698cd49b641
IslemBacha <124149772+IslemBacha@users.noreply.github.com>
2023-05-20 22:56:33 +0200

    first commit

---
+++ src/Component/ListMovie.js
@@ -0,0 +1,14 @@
+import CardMovie from "./CardMovie"
+
+const ListMovie=({movies,search,rate, setMovies})=>{
+  const x =  movies.filter(el=> el.title.toUpperCase().includes(search .toUpperCase())
+  && el.rating>=rate). map(el=> <CardMovie el={el} movies={movies} setMovies={setMovies}/>)
+    return(
+        <div className="islem">
+          {
+          x.length> 0 ? x :<h1>Not Found </h1>
+          }
+        </div>
+    )
+}
+export default ListMovie
